36402b2bca4620df77b3fd2f49bc4c7ffdcc75e1
Saman <saman.ashoori@outlook.com>
2026-04-22 22:01:28 +0100

    fixed rotation bug

--- src/main.c
+++ src/main.c
@@ -87,12 +87,16 @@ vec2_t project(vec3_t point){
 
 void update(void)
 {
-	cube_rotation.x += 0.005;
+	cube_rotation.x += 0.01;
+	cube_rotation.y += 0.01;
+	cube_rotation.z += 0.01;
 	for (int i = 0;i< N_POINTS;i++){
 		vec3_t point = cube_points[i];
 
 		//rotation here
 		vec3_t transformed_point = vec3_rotate_x(point,cube_rotation.x);
+		transformed_point = vec3_rotate_y(transformed_point,cube_rotation.y);
+		//transformed_point = vec3_rotate_z(transformed_point,cube_rotation.z);
 
 		//Translating the point away from the camera
 		transformed_point.z -= camera_position.z;
