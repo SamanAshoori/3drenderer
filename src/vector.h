3f2838ae59d795b64b047d5f0b426ba19bceedd6
Saman <saman.ashoori@outlook.com>
2026-07-17 21:59:48 +0100

    getting cross product of a vector

--- src/vector.h
+++ src/vector.h
@@ -33,6 +33,7 @@ vec3_t vec3_add(vec3_t a, vec3_t b);
 vec3_t vec3_subtract(vec3_t a, vec3_t b);
 vec3_t vec3_multiply(vec3_t v, float factor);
 vec3_t vec3_divide(vec3_t v, float factor);
+vec3_t vec3_cross(vec3_t a,vec3_t b);
 
 
 #endif
