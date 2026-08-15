2126befc4aea4f30960f61eb014a0c75b8a81ab5
Saman <saman.ashoori@outlook.com>
2026-08-15 15:45:48 +0100

    fix divide by zero error

--- src/triangle.c
+++ src/triangle.c
@@ -62,15 +62,23 @@ void draw_filled_triangle(int x0,int y0,int x1,int y1,int x2, int y2,uint32_t co
         int_swap(&x0,&x1);
     }
 
-    //calculate new vertex (mX,mY) using triangle similarity
-    int My = y1;
-    int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2-y0)) + x0;
+    if (y1 == y2){
+        //can simply just draw flat bottom
+        fill_flat_bottom_triangle(x0,y0,x1,y1,x2,y2,colour);
+    } 
+    else if (y0 == y1) {
+        //can simply draw flat top
+        fill_flat_top_triangle(x0,y0,x1,y1,x2,y2,colour);
+    } 
+    else{
+        //calculate new vertex (mX,mY) using triangle similarity
+        int My = y1;
+        int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2-y0)) + x0;
 
-    
-    //draw flat-bottom 
-    fill_flat_bottom_triangle(x0,y0,x1,y1,Mx,My,colour);
+        //draw flat-bottom 
+        fill_flat_bottom_triangle(x0,y0,x1,y1,Mx,My,colour);
 
-    //draw flat top 
-    fill_flat_top_triangle(x1,y1,Mx,My,x2,y2,colour);
-
+        //draw flat top 
+        fill_flat_top_triangle(x1,y1,Mx,My,x2,y2,colour);
+    }
 }
